34cb717f290eed17dd1378161ea22f99dfa37afe
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2024-10-19 15:35:45 +0530

    feat: the uglyness of React.createElement, which is why JSX got invented.

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-const reactElement = React.createElement('h1', {id: "heading", xyz: "xyz"}, "Hello World from React!!! Fuck the React.");
+const reactElement = React.createElement('h1', { id: "heading", xyz: "xyz" }, "Hello World from React!!! Fuck the React.");
+
+const parent = React.createElement('div', { id: 'parent' }, [
+    React.createElement('div', { id: 'child1' }, [
+        React.createElement('h1', {}, "Heading 1"), 
+        React.createElement('h2', {}, "Heading 2")
+    ]),
+    React.createElement('div', { id: 'child2' }, [
+        React.createElement('h1', {}, "Heading 1"), 
+        React.createElement('h2', {}, "Heading 2")
+    ])
+])
 
 console.log(reactElement); // this will return an object, not the HTML tag
 
 const reactDOMRoot = ReactDOM.createRoot(document.getElementById("root"));
-
-reactDOMRoot.render(reactElement)
+// reactDOMRoot.render(parent)
+reactDOMRoot.render(parent, reactElement) //
