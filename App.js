da3ce9ee9bd612ff1e12bee8b0992450c55dd41d
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2024-10-19 19:38:36 +0530

    feat: making my own create-react-appgit add . Fuck the Reactgit add .

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement('div', { id: 'parent' }, [
     React.createElement('div', { id: 'child1' }, [
         React.createElement('h1', {}, "Heading 1"), 
@@ -7,7 +10,7 @@ const parent = React.createElement('div', { id: 'parent' }, [
         React.createElement('h1', {}, "Heading 1"), 
         React.createElement('h2', {}, "Heading 2")
     ])
-]) 
+])
 
 const reactDOMRoot = ReactDOM.createRoot(document.getElementById("root"));
 const reactDOMRoot1 = ReactDOM.createRoot(document.getElementById("root1"))
