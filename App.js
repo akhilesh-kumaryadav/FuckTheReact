a3135eb3010c134c39c1dfbbf62e60c5082fb0c7
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2024-10-20 13:02:25 +0530

    feat: react functional component, all the forms, how they are rendered

--- App.js
+++ App.js
@@ -1,8 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const headingJSX = (<div><h1 id="heading" tabIndex='5'>Heading using JSX!! Fuck the React!!!</h1></div>);
+//React Component
+const headingJSX = (
+  <div>
+    <h1 id="heading" tabIndex="5">
+      Heading using JSX Component, industry standard code!! Fuck the React!!!
+    </h1>
+  </div>
+);
 
-const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
+//React Funcational Componenet
+const HeadingJSXComponent = () => (
+  <div>
+    <h1 id="heading" tabIndex="5">
+      Heading using JSX Component, industry standard code!! Fuck the React!!!
+    </h1>
+  </div>
+);
 
-reactRoot.render(headingJSX)
+const HeadingJSXComponent2 = () => {
+  return (
+    <div>
+      <h1 id="heading" tabIndex="5">
+        Heading using JSX, more explaible code!! Fuck the React!!!
+      </h1>
+    </div>
+  );
+};
+
+const HeadingJSXComponent3 = () => <h1>Heading in single line</h1>;
+
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+reactRoot.render(<HeadingJSXComponent />);
