c787aaf2a955952926b72b4142794afff321b8d9
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2024-10-20 11:41:31 +0530

    feat: playing with JSX

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const headingJSX = <h1 id="heading">Heading using JSX!! Fuck the React!!!</h1>
+const headingJSX = (<div><h1 id="heading" tabIndex='5'>Heading using JSX!! Fuck the React!!!</h1></div>);
 
 const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
 
