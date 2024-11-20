6198f582f2d3f6a05923fabbbb8aac8439e9e04a
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2024-11-20 08:18:48 +0530

    feat: class based components intro

---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+  return (
+    <div className="user">
+      <h2>AKhilesh</h2>
+      <p>Location - Kanpur</p>
+      <p>Contact Number - 7522089744</p>
+    </div>
+  );
+};
+
+export default User;
