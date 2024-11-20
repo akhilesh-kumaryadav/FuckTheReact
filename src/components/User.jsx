7fde4134037b6d61f3c59ad5a513ee54b2bd5fe0
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2024-11-20 08:25:15 +0530

    feat: declaring state variable in class based component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 const User = () => {
+  const [count0] = useState(0);
+  const [count1] = useState(1);
+
   return (
     <div className="user">
+      <p>count0 - {count0}</p>
+      <p>count1 - {count1}</p>
       <h2>AKhilesh</h2>
       <p>Location - Kanpur</p>
       <p>Contact Number - 7522089744</p>
