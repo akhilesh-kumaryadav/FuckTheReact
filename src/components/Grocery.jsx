89cafd9bb8a29a7ece8f071ff4b35d06c1536e29
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2025-02-03 21:21:14 +0530

    feat: lazy loading for the grocery component

---
+++ src/components/Grocery.jsx
@@ -0,0 +1,10 @@
+const Grocery = () => {
+  return (
+    <h1>
+      This is Grocery page. It has alot of other children component which very
+      heavy....
+    </h1>
+  );
+};
+
+export default Grocery;
