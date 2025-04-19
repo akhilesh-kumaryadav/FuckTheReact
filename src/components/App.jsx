e04e3aa2175b61e2140ebd7f879b4e9a993149e6
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2025-04-19 17:35:53 +0530

    feat: adding the redux for subscribing to the cart items - adjusting the arrow of the list item category of the restaurant

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import UserContext from "../utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
 
 const App = () => {
   const [userName, setUserName] = useState();
@@ -16,13 +18,15 @@ const App = () => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-      <div id="app" className="app">
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div id="app" className="app">
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 
