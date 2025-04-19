b3dfb9e5e69910ef51fcf1c6edaf66bdf63baaeb
Akhilesh Yadav <akhileshkumaryadav2407@gmail.com>
2025-04-19 18:29:57 +0530

    feat: do not display the clear cart button when the cart is empty

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,12 +13,17 @@ const Cart = () => {
   return (
     <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart</h1>
-      <button
-        className="bg-red-500 font-bold m-2 p-2 rounded-2xl"
-        onClick={handleClearCart}
-      >
-        Clear Cart
-      </button>
+      {cartItems.length ? (
+        <button
+          className="bg-red-500 font-bold m-2 p-2 rounded-2xl"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+      ) : (
+        <h1>Please items to the cart.</h1>
+      )}
+
       <div className="w-6/12 m-auto">
         <ItemList data={cartItems} />
       </div>
